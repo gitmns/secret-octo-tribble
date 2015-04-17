a4efda12ca217f49aebd90c1ee98e22505e84c94
manas.pattanaik <mail.mantuu@gmail.com>
2015-04-17 12:06:06 +0530

    initial commit

---
+++ Log4NetExe/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Here is the once-per-application setup information
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "MyStandardLog4Net.config", Watch = true)]
+
+namespace Log4NetExe
+{
+    class Program
+    {
+        //Here is the once-per-class call to initialize the log object
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                throw new System.IO.FileNotFoundException();
+            }
+
+            catch (Exception ex)
+            {
+                log.Error("Error error logging", ex);
+            }
+        }
+    }
+}
